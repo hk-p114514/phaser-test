836c79e06486fef70ded127b309615dfbf313b09
hk-p114514 <hukada.gogotea@gmail.com>
2021-12-06 00:10:19 +0900

    webpack5の画像バンドルのサンプルを入手

---
+++ phaser-test/paths.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+	// source directory
+	src: path.join(__dirname, './src'),
+
+	// production directory
+	dist: path.join(__dirname, './dist'),
+
+	// public directory
+	public: path.join(__dirname, './public'),
+};
